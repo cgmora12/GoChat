129eb2676bbb4e59c78dd88cd0722c19fa8bb763
cgmora12 <cesar_realmadrid_12@hotmail.com>
2016-03-02 09:53:47 +0100

    Fichero inicial
    Probando ramas en github

---
+++ ejemplo.go
@@ -0,0 +1,27 @@
+/*
+Ejemplo 1
+
+Este programa copia de la entrada a la salida carácter a carácter,
+restringiéndose a un alfabeto limitado y pasando a mayúsculas.
+Permite leer de la entrada y salida estándar o usar ficheros
+
+ejemplos de uso:
+
+go run ejemplo1.go
+
+go run ejemplo1.go fichentrada.txt fichsalida.txt
+
+
+-lectura y escritura
+-entrada y salida estándar
+-ficheros
+-parámetros en línea de comandos (os.Args)
+*/
+
+package main
+
+import ()
+
+func main() {
+
+}
